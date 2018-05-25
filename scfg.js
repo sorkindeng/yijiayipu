637ec173decc1bb22a13d2664315ab0ac4d502ea
Sorkin <sorkindeng@163.com>
2018-05-25 10:30:11 +0800

    add scfg.js

---
+++ scfg.js
@@ -0,0 +1,20 @@
+
+
+const secretConfig = {
+  // 启动端口
+  port: 3000,
+  // 数据库配置
+  database: {
+    DATABASE: '',
+    USERNAME: '',
+    PASSWORD: '',
+    PORT: '3306',
+    HOST: 'localhost'
+  },
+
+  LEANCLOUD_APP_ID : '',
+  LEANCLOUD_APP_KEY :'',
+  
+}
+
+module.exports = secretConfig
