fad1bc55bae641f4d01c5e5e2c69be975dda05f0
Sorkin <sorkindeng@163.com>
2018-05-25 10:46:09 +0800

    commit local

--- scfg.js
+++ scfg.js
@@ -1,19 +1,24 @@
-
+//
+//  LeanCloud App:yijiayipu
+//  App ID=hnVQQe0BccOx11v1qNyMgSJ3-gzGzoHsz
+//  App Key=6U0sie3qndINrG3iXBxUuSC2
+//  Master Key=7DfAXo2jvVAJibcsd6dfbDwG
+//
 
 const secretConfig = {
   // 启动端口
   port: 3000,
   // 数据库配置
   database: {
-    DATABASE: '',
-    USERNAME: '',
-    PASSWORD: '',
+    DATABASE: 'yijiayipu',
+    USERNAME: 'yipu',
+    PASSWORD: 'yyp2018',
     PORT: '3306',
     HOST: 'localhost'
   },
 
-  LEANCLOUD_APP_ID : '',
-  LEANCLOUD_APP_KEY :'',
+  LEANCLOUD_APP_ID : 'hnVQQe0BccOx11v1qNyMgSJ3-gzGzoHsz',
+  LEANCLOUD_APP_KEY :'6U0sie3qndINrG3iXBxUuSC2',
   
 }
 
